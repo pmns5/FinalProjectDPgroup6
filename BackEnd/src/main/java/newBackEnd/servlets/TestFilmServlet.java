d9a97472d8bf872ac18aae4c031d4e32a65f5e21
pmns5 <p.mansi5@studenti.unisa.it>
2021-12-06 11:53:34 +0100

    Query

--- BackEnd/src/main/java/newBackEnd/servlets/TestFilmServlet.java
+++ BackEnd/src/main/java/newBackEnd/servlets/TestFilmServlet.java
@@ -30,43 +30,6 @@ import java.util.List;
 @MultipartConfig(maxFileSize = 16177215)
 public class TestFilmServlet extends HttpServlet {
 
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//        String firstName = request.getParameter("title");
-//        String lastName = request.getParameter("plot");
-//        String[] actors = request.getParameterValues("actors");
-//
-//
-//        InputStream inputStream = null; // input stream of the upload file
-//        // obtains the upload file part in this multipart request
-//        Part filePart = request.getPart("poster");
-//        byte[] image = null;
-//        if (filePart != null) {
-//            // prints out some information for debugging
-//            System.out.println(filePart.getName());
-//            System.out.println(filePart.getSize());
-//            System.out.println(filePart.getContentType());
-//
-//            //obtains input stream of the upload file
-//            //the InputStream will point to a stream that contains
-//            //the contents of the file
-//            inputStream = filePart.getInputStream();
-//            image = inputStream.readAllBytes();
-//        }
-//
-//
-//        ServletContext sc = getServletContext();
-//        String filepath = "C:\\Users\\pmans\\IdeaProjects\\FilmReviewProject\\BackEnd\\src\\main\\webapp\\src\\manager\\images\\image.jpeg";
-//
-//        byte[] fileContent = FileUtils.readFileToByteArray(new File(filepath));
-//        String encodedString = Base64.getEncoder().encodeToString(image);
-//        OutputStream os = response.getOutputStream();
-//        response.setContentType("image/jpeg");
-//        os.write(encodedString.getBytes());
-//
-//    }
-//
-
     private FilmDecorator db;
 
     @Override
@@ -182,4 +145,5 @@ public class TestFilmServlet extends HttpServlet {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         }
     }
+
 }
