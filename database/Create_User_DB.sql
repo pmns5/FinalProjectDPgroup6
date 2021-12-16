be379af7a4d4414208391b696122a80d2ed0cf3a
vince <v.salvati10@studenti.unisa.it>
2021-12-16 17:39:53 +0100

    queries

--- database/Create_User_DB.sql
+++ database/Create_User_DB.sql
@@ -7,8 +7,8 @@ DROP TABLE IF EXISTS user CASCADE;
 CREATE TABLE user
 (
     id_user       INT AUTO_INCREMENT PRIMARY KEY,
-    username      VARCHAR(20),
-    email         VARCHAR(20),
+    username      VARCHAR(20) UNIQUE,
+    email         VARCHAR(20) UNIQUE,
     user_password VARCHAR(20),
     ban           INT
 );
