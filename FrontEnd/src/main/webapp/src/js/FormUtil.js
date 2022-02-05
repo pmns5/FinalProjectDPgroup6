767a907302ddf77e0f6cd0b181d7943ad68e718f
giuseppe98 <G.RENZULL4@studenti.unisa.it>
2022-02-05 20:24:35 +0100

    New Frontend

--- FrontEnd/src/main/webapp/src/js/FormUtil.js
+++ FrontEnd/src/main/webapp/src/js/FormUtil.js
@@ -1,22 +1,26 @@
 function validate(idForm) {
-    return true;
-    $(idForm).addClass("needs-validation")
-    let flag = true
-    let forms = document.getElementsByClassName("needs-validation");
-    Array.prototype.filter.call(forms, function (form) {
-        if (form.checkValidity() === false) {
-            flag = false
-        }
-    }, false)
-    $(idForm).removeClass("needs-validation")
-    return flag
+  return true;
+  $(idForm).addClass("needs-validation");
+  let flag = true;
+  let forms = document.getElementsByClassName("needs-validation");
+  Array.prototype.filter.call(
+    forms,
+    function (form) {
+      if (form.checkValidity() === false) {
+        flag = false;
+      }
+    },
+    false
+  );
+  $(idForm).removeClass("needs-validation");
+  return flag;
 }
 
 const Genres = {
-    ACTION: 'Action',
-    ADVENTURE: 'Adventure',
-    COMEDY: 'Comedy',
-    HORROR: 'Horror',
-    ROMANCE: 'Romance'
-}
-Object.freeze(Genres)
+  ACTION: "Action",
+  ADVENTURE: "Adventure",
+  COMEDY: "Comedy",
+  HORROR: "Horror",
+  ROMANCE: "Romance",
+};
+Object.freeze(Genres);
--- FrontEnd/src/main/webapp/src/js/LoginPage.js
+++ FrontEnd/src/main/webapp/src/js/LoginPage.js
@@ -1,66 +1,70 @@
 class LoginPage {
-    constructor(endpoint) {
-        this.addUserEndpoint = endpoint + "/add-user";
-        this.loginUserEndpoint = endpoint + "/login-user";
+  constructor(endpoint) {
+    this.addUserEndpoint = endpoint + "/add-user";
+    this.loginUserEndpoint = endpoint + "/login-user";
+  }
+
+  registration() {
+    let controller = this;
+    let data = $("#register-form").serialize();
+    $.ajax({
+      type: "post",
+      url: controller.addUserEndpoint,
+      data: data,
+    })
+      .done(function () {
+        controller.reload();
+      })
+      .fail(function () {
+        controller.reload();
+      });
+  }
+
+  access() {
+    let controller = this;
+    let data = $("#login-form").serialize();
+    $.ajax({
+      type: "post",
+      url: controller.loginUserEndpoint,
+      data: data,
+      traditional: true,
+    })
+      .done(function (data) {
+        data = JSON.parse(data);
+        setCookie(data, 1);
+        controller.redirect();
+      })
+      .fail(function () {
+        controller.reload();
+      });
+  }
+
+  redirect() {
+    let role = getCookieRole();
+    if (role === "client") {
+      $(location).attr("href", "client/MainPageClient.html");
+    } else {
+      $(location).attr("href", "manager/MainPageManager.html");
     }
+  }
 
-    registration() {
-        let controller = this;
-        let data = $('#register-form').serialize();
-        $.ajax({
-            type: 'post',
-            url: controller.addUserEndpoint,
-            data: data,
-        }).done(function () {
-            controller.reload();
-        }).fail(function () {
-            controller.reload();
-        })
+  reload() {
+    $(location).attr("href", "index.html");
+  }
+
+  renderAlert(message, success) {
+    let alert;
+    if (success) {
+      alert = $("#success-alert-template");
+    } else {
+      alert = $("#fail-alert-template");
     }
-
-    access() {
-        let controller = this;
-        let data = $('#login-form').serialize();
-        $.ajax({
-            type: 'post',
-            url: controller.loginUserEndpoint,
-            data: data,
-            traditional: true,
-        }).done(function (data) {
-            data = JSON.parse(data)
-            setCookie(data, 1);
-            controller.redirect();
-        }).fail(function () {
-            controller.reload();
-        })
-    }
-
-    redirect() {
-        let role = getCookieRole();
-        if (role === "client") {
-            $(location).attr("href", "client/MainPageClient.html");
-
-        } else {
-            $(location).attr("href", "manager/MainPageManager.html");
-        }
-    }
-
-    reload() {
-        $(location).attr("href", "index.html");
-    }
-
-    renderAlert(message, success) {
-        let alert;
-        if (success) {
-            alert = $('#success-alert-template');
-        } else {
-            alert = $('#fail-alert-template');
-        }
-        const html = alert.html().replace(/{message}/ig, message);
-        $(html).prependTo('#response-alert-section')
-            .delay(5000)
-            .queue(function () {
-                $(this).remove();
-            });
-    };
+    const html = alert.html().replace(/{message}/gi, message);
+    $(html)
+      .prependTo("#response-alert-section")
+      .delay(5000)
+      .queue(function () {
+        $(this).remove();
+      });
+  }
 }
