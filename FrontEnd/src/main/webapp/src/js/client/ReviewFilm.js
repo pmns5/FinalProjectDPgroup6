d5ff5c26e3e6dcdad41f641e9a1a381eb67cd48d
pmns5 <p.mansi5@studenti.unisa.it>
2022-02-06 16:17:17 +0100

    Restyling

--- FrontEnd/src/main/webapp/src/js/client/ReviewFilm.js
+++ FrontEnd/src/main/webapp/src/js/client/ReviewFilm.js
@@ -51,16 +51,19 @@ class ReviewFilm {
     let static_film = $("#film-template").html()
     $.each(data, function (index, obj) {
       let film_view = static_film
-      console.log(obj.id);
+      
 
       film_view = film_view.replace(/{ID}/gi, obj.id);
       film_view = film_view.replace(/{POSTER}/gi, obj.poster)
       film_view = film_view.replace(/{TITLE}/gi, obj.title)
       film_view = film_view.replace(/{ACTORS}/gi, addActors(obj.actors))
       let avg = ((Number)(obj.avgScore)).toFixed(2);
-      film_view = film_view.replace(/{SCORE}/gi,(avg==0 ? "Be the first to comment" : avg));
-      
 
+      if(avg>=5){
+        
+      }
+
+      film_view = film_view.replace(/{SCORE}/gi,(avg==0 ? "Be the first to comment" : avg));
       if (controller.count === 0) array.push('<div class="col-sm-12 row">');
 
       array.push(film_view);
@@ -72,38 +75,6 @@ class ReviewFilm {
     controller.count = 0;
   }
 
-  constructFilmView(obj) {
-    let cutPlot;
-    if (obj.plot.length > PLOT_LENGTH) {
-      cutPlot = obj.plot.substr(0, PLOT_LENGTH) + "...";
-    } else {
-      cutPlot = obj.plot;
-    }
-    return (
-      '<div class="col-sm-4">' +
-      '   <div class="our_2">' +
-      '<div class="ih-item square effect5 left_to_right" > ' +
-      '<a data-target="#edit-modal" data-toggle="modal" onclick="controller.viewEditFilm(' +
-      obj.id +
-      ')">' +
-      "           <img src=data:image/jpeg;base64," +
-      obj.poster +
-      ' alt="img" >' +
-      '            <div class="info">' +
-      "               <h3>" +
-      obj.title +
-      "</h3>" +
-      '               <p style="font-size: small">' +
-      cutPlot +
-      "</p>" +
-      addActors(obj.actors) +
-      addStars(obj.avgScore) +
-      "            </div>" +
-      "       </a></div>" +
-      "   </div>" +
-      "</div>"
-    );
-  }
 
   renderAlert(message, success) {
     let alert;
