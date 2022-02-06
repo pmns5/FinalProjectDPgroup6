88602e99950797b33625e04cbe2515d423a41dbe
pmns5 <p.mansi5@studenti.unisa.it>
2022-02-06 16:32:19 +0100

    Restyling

---
+++ FrontEnd/target/FrontEnd-1.0-SNAPSHOT/src/js/UserEdit.js
@@ -0,0 +1,60 @@
+class UserEdit {
+  constructor(endPoint) {
+    this.editEndPoint = endPoint + "/edit-user";
+    this.deleteEndPoint = endPoint + "/delete-user";
+    this.getEndPoint = endPoint + "/get-user";
+  }
+
+  fillTable() {
+    let controller = this;
+    $.getJSON(this.getEndPoint, { id_user: getCookieID() }, function (data) {
+      $("#id_user").val(getCookieID());
+      $("#role").val(getCookieRole());
+      $("#password").val(data.password);
+      $("#email").val(data.email);
+      $("#username").val(data.username);
+    });
+    if (getCookieRole() === "admin") {
+      $("#delete-button").prop("hidden", true);
+    }
+  }
+
+  editUser() {
+    let controller = this;
+    let newUsername = $('#username').val();
+    let data = $("#entry-form").serialize();
+    $.post(controller.editEndPoint, data, function () {})
+      .done(function (msg) {
+        updateCookieUsername(newUsername);
+        controller.renderAlert("Account successfully edit");
+        window.location.reload(true);
+      })
+      .fail(function (msg) {
+        controller.renderAlert("Error while editing this account!! Try again.");
+      });
+  }
+
+  deleteUser() {
+    $.get(this.deleteEndPoint, { id_user: getCookieID() }, function () {
+
+    });
+    $(location).attr("href", "./index.html");
+    document.cookie = "data=" + null + ";expires=" + 0 + ";path=/";
+  }
+
+  renderAlert(message, success) {
+    let alert;
+    if (success) {
+      alert = $("#success-alert-template");
+    } else {
+      alert = $("#fail-alert-template");
+    }
+    const html = alert.html().replace(/{message}/gi, message);
+    $(html)
+      .prependTo("#response-alert-section")
+      .delay(5000)
+      .queue(function () {
+        $(this).remove();
+      });
+  }
+}
